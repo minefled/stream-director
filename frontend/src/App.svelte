f59f9ee5d0f02b6bf63321fede486f69bda6edcc
minefled <florian-martin@stembal.de>
2022-02-05 22:15:21 +0100

    More Project setup

---
+++ frontend/src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Header from "./components/general/header/Header.svelte";
+	import Scenes from "./components/general/scenes/Scenes.svelte";
+	import CategoryList from "./components/general/streamelements/CategoryList.svelte";
+</script>
+
+<div class="app">
+	<Header />
+
+	<div class="content">
+		<Scenes />
+		<CategoryList />
+	</div>
+</div>
+
+<style>
+	.app {
+		width: 100%;
+		height: 100vh;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.content {
+		width: 100%;
+		height: calc(100% - 50px);
+
+		display: flex;
+		flex-direction: row;
+	}
+</style>
